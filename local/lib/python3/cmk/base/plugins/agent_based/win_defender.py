2b9dd6324cc082a3d93d4a5e2ec2a269e8dceb84
Jan-Hendrik Scholz <jan-hendrikscholz@outlook.com>
2021-10-05 21:50:17 +0200

    Create check

---
+++ local/lib/python3/cmk/base/plugins/agent_based/win_defender.py
@@ -0,0 +1,39 @@
+from .agent_based_api.v1 import *
+import pprint
+
+
+def discover_win_defender(section):
+        yield Service(item="Windows Defender")
+
+
+def check_win_defender(item,section):
+        for rtprot_status, as_age, av_age, am_productversion, am_engineversion in section:
+                as_age= int((as_age.replace(" ","")),10)
+                av_age= int((av_age.replace(" ","")),10)
+                age = int(0)
+                if as_age > 3:
+                        age + 1
+                if av_age > 3:
+                        age + 1
+                if rtprot_status == "False":
+                        if age > 0:
+                                yield Result(state=State.CRIT, summary="RealTime Protection: Disabled(!!), Anti-virus Database: Out-of-Date(!!), AMProductVersion:" + am_productversion + ", AMEngineVersion:" + am_engineversion)
+                                return
+                        else:
+                                yield Result(state=State.CRIT, summary="RealTime Protection: Disabled(!!), Anti-virus Database: Up-to-Date, AMProductVersion:" + am_productversion + ", AMEngineVersion:" + am_engineversion)
+                                return
+                else:
+                        if age > 0:
+                                yield Result(state=State.CRIT, summary="RealTime Protection: Enabled, Anti-virus Database: Out-of-Date(!!), AMProductVersion:" + am_productversion + ", AMEngineVersion:" + am_engineversion)
+                                return
+                        else:
+                                yield Result(state=State.OK, summary="RealTime Protection: Enabled, Anti-virus Database: Up-to-date, AMProductVersion:" + am_productversion + ", AMEngineVersion:" + am_engineversion)
+                                return
+
+
+register.check_plugin(
+    name = "win_defender",
+    service_name = "%s",
+    discovery_function = discover_win_defender,
+    check_function = check_win_defender,
+)
