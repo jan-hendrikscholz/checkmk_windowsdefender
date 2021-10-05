a7f134241ca12032fe0b06fc1158691013f3aca0
Jan-Hendrik Scholz <jan-hendrikscholz@outlook.com>
2021-10-05 21:51:32 +0200

    Create agent plugin

---
+++ local/share/check_mk/agents/windows/plugins/win_defender.ps1
@@ -0,0 +1,17 @@
+#################################################################
+## Windows Defender
+##################################################################
+## Created By: Kevin Tijssen
+## Last updated by: Jan-Hendrik Scholz
+##################################################################
+$ErrorActionPreference = "SilentlyContinue"
+$DefenderData= Get-MpComputerStatus
+$RTP = $DefenderData.RealTimeProtectionEnabled
+$AS_Age = $DefenderData.AntispywareSignatureAge
+$AV_Age = $DefenderData.AntivirusSignatureAge
+$AM_ProductVersion = $DefenderData.AMProductVersion
+$AM_EngineVersion = $DefenderData.AMEngineVersion
+if ($null -ne $DefenderData){
+        Write-Host "<<<win_defender:sep(9)>>>"
+        Write-Host $RTP "`t" $AS_Age "`t" $AV_Age "`t" $AM_ProductVersion "`t" $AM_EngineVersion
+    }
